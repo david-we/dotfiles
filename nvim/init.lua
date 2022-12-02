62700af4317c8f5adb2d0c7affc84523b825740c
david-we <david.weidhofer@gmail.com>
2022-12-02 10:10:34 +0100

    initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,5 @@
+require("dwe.base") -- General Settings
+require("dwe.highlights") -- Colourscheme and other highlights
+require("dwe.maps") -- Keymaps
+require("dwe.plugins") -- Plugins
+require("dwe.bootstrap") -- Packer Auto-Installer
---
+++ nvim/lua/dwe/highlights.lua
@@ -0,0 +1,16 @@
+-------------------------------------------------
+-- COLORSCHEMES
+-------------------------------------------------
+
+-- Uncomment just ONE of the following colorschemes!
+local ok, _ = pcall(vim.cmd, "colorscheme tokyonight-storm")
+--local ok, _ = pcall(vim.cmd, "colorscheme tokyonight-night")
+--local ok, _ = pcall(vim.cmd, "colorscheme tokyonight-day")
+
+-- Highlight the region on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = num_au,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 120 })
+	end,
+})
